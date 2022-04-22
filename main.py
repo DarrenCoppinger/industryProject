90704048784163704fc6e26fab302ecbc2908c4c
DarrenCoppinger <darrencoppinger@gmail.com>
2022-04-22 22:37:48 +0100

    Add requirements.txt

--- main.py
+++ main.py
@@ -3,6 +3,12 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+# import PyAudio
+import sys
+
+import pyaudio
+import keyboard
+
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -11,7 +17,37 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-    print_hi('How are you?')
+    CHUNK = 1024
+    WIDTH = 2
+    CHANNELS = 2
+    RATE = 44100
+    RECORD_SECONDS = 5
+
+    pa = pyaudio.PyAudio()
+
+    stream = pa.open(format=pa.get_format_from_width(WIDTH),
+                    channels=CHANNELS,
+                    rate=RATE,
+                    input=True,
+                    output=True,
+                    frames_per_buffer=CHUNK)
+
+    frames = []
+
+    print("* recording")
+
+    for i in range(0, int(RATE / CHUNK * sys.maxunicode)):
+        data = stream.read(CHUNK)
+        stream.write(data, CHUNK)
+
+    print("* done")
+
+    stream.stop_stream()
+    stream.close()
+
+    pa.terminate()
+
+    print("Live noise-cancelling mode")
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
