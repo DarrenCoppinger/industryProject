5d425a687289ba609e826c11f88f14b687080d11
DarrenCoppinger <darrencoppinger@gmail.com>
2022-04-24 22:43:09 +0100

    create input and output and stream and invert output

--- main.py
+++ main.py
@@ -8,11 +8,25 @@ import sys
 
 import pyaudio
 import keyboard
+import numpy as np
 
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+def inverter(data):
+    # Convert byte string into integer
+    wave = np.frombuffer(data, np.int16)
+
+    # invert integer using NumPy
+    invert_wave = np.invert(wave)
+
+    # Convert integer back into byte string
+    inverted_sound = np.frombuffer(invert_wave, np.byte)
+
+    return inverted_sound
 
 
 # Press the green button in the gutter to run the script.
@@ -23,9 +37,9 @@ if __name__ == '__main__':
     RATE = 44100
     RECORD_SECONDS = 5
 
-    pa = pyaudio.PyAudio()
+    p = pyaudio.PyAudio()
 
-    stream = pa.open(format=pa.get_format_from_width(WIDTH),
+    stream = p.open(format=p.get_format_from_width(WIDTH),
                     channels=CHANNELS,
                     rate=RATE,
                     input=True,
@@ -35,19 +49,32 @@ if __name__ == '__main__':
     frames = []
 
     print("* recording")
+    try:
+        #     for i in range(0, int(RATE / CHUNK * sys.maxunicode)):
+        for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+            input_stream = stream.read(CHUNK)
+            stream.write(input_stream, CHUNK)
 
-    for i in range(0, int(RATE / CHUNK * sys.maxunicode)):
-        data = stream.read(CHUNK)
-        stream.write(data, CHUNK)
+            # Add to frame array
+            frames.append(input_stream)
+            # invert the wave
+            inverted_stream = inverter(input_stream)
+            print("i= ", i)
+            if keyboard.is_pressed('x'):
+                print("You pressed x")
+                break
+    except (KeyboardInterrupt, SystemExit):
+        print('Exit Live noise cancelling')
+    except Exception as e:
+        print(str(e))
 
     print("* done")
 
     stream.stop_stream()
     stream.close()
 
-    pa.terminate()
+    p.terminate()
 
     print("Live noise-cancelling mode")
-
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
