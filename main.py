5a01ed650e308adedd0a96150706a465729c177f
DarrenCoppinger <darrencoppinger@gmail.com>
2022-04-26 16:14:03 +0100

    add noisy to input signals

--- main.py
+++ main.py
@@ -10,7 +10,10 @@ import numpy as np
 import wave
 import matplotlib.pyplot as plt
 
-CHUNK = 1024
+import struct
+
+# CHUNK = 1024
+CHUNK = 2
 WIDTH = 2
 CHANNELS = 2
 RATE = 44100
@@ -19,6 +22,9 @@ RECORD_SECONDS = 5
 # Create pyaudio object
 p = pyaudio.PyAudio()
 
+# Output variables
+WAVE_OUTPUT_FILENAME = "output.wav"
+FORMAT = pyaudio.paInt16
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -81,23 +87,79 @@ def file_anc():
     filename = input("Enter WAV file name: ")
     (wavefile, stream, dt, w_len) = read_file(filename)
     print("Reading WAV file")
+    print("wavefile channels", wavefile.getnchannels())
+    # plt.plot(wavefile)
 
     # read first byte CHUNK of wav file
     file_data = wavefile.readframes(CHUNK)
 
     # create matrix of wave data
     input_array = []
+    noise_array = []
+    input_plus_noise_array = []
+
+    output_frames = []
     inverted_array = []
+
+    # calculate time series
+    t = np.arange(0, w_len, (dt*CHUNK))
 
     # read in byte from wave file
     while file_data != b'':
         try:
             # Play audio from file
-            stream.write(file_data)
+            # stream.write(file_data)
+            # print("file_data=", file_data)
 
-            # save data to array
-            input_data = np.frombuffer(file_data, np.int16)[0]
-            input_array.append(input_data)
+            # covert byte data to integer data
+            # input_data_int = np.frombuffer(file_data, np.int16)[0]
+            # input_data_int = np.frombuffer(file_data, np.int16)
+            # input_data_int = int.from_bytes(file_data, sys.byteorder)
+            input_data_int = np.frombuffer(file_data, np.int16)
+
+            # covert int data back to byte data
+            # input_data_byte = input_data_int.to_bytes(CHUNK, sys.byteorder)
+            # input_data_byte = np.frombuffer(input_data_int, np.byte)
+            # input_data_byte = struct.pack(input_data_int)
+
+            # # stream.write(input_data_byte)
+
+            # input_data = np.frombuffer(file_data, np.int16)
+            # print("input_data= ", input_data)
+            # add integer data to array
+            input_array.append(input_data_int)
+
+            # create noise
+            noise = np.random.randint(-1000, 1000)
+            # output noise data to array
+            noise_array.append(noise)
+            # print("noise= ", noise)
+
+            # add noise to input audio
+            input_plus_noise = input_data_int + noise
+
+            # output input and noise data to array
+            input_plus_noise_array.append(input_plus_noise)
+
+            # convert from integer back to bytes
+            input_plus_noise_byte = np.frombuffer(input_plus_noise, np.byte)
+            stream.write(input_plus_noise_byte)
+
+            # # input_plus_noise_bytes = input_plus_noise.tobytes()
+            # input_plus_noise_bytes = np.frombuffer(input_plus_noise, np.byte)
+            # input_plus_noise_wave = np.frombuffer(input_plus_noise, np.byte)
+
+            # output byte data to array
+            output_frames.append(input_plus_noise_byte)
+
+
+            # print("input_plus_noise= ", input_plus_noise)
+            # int_input_plus_noise = np.frombuffer(input_plus_noise, np.int16)[0]
+
+            # print("int_input_plus_noise= ", int_input_plus_noise)
+            # input_plus_noise_array.append(int_input_plus_noise)
+
+            # input_plus_noise_data = np.frombuffer(input_plus_noise, np.int16)[0]
 
             # Read next chuck of data
             file_data = wavefile.readframes(CHUNK)
@@ -111,16 +173,26 @@ def file_anc():
         except Exception as e:
             print(str(e))
 
-
-
     # Close stream after finishing reading data
     stream.stop_stream()
     stream.close()
 
     print("End of WAV file")
 
-    # plot wave
-    plot_data(input_array, dt, w_len)
+    # plot input wave
+    # plot_data(input_array, input_plus_noise_array, noise_array, t)
+
+    # create output file
+    wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+    wf.setnchannels(wavefile.getnchannels())
+    wf.setsampwidth(wavefile.getsampwidth())
+    wf.setframerate(wavefile.getframerate())
+    wf.writeframes(b''.join(output_frames))
+    wf.close()
+
+
+    # plot input plus noise wave
+    # plot_data(input_array, t, "noisy")
 
     # Terminate PyAudio object
     p.terminate()
@@ -152,11 +224,15 @@ def read_file(name):
     return wf, stream, dt, w_len
 
 
-def plot_data(array, dt, length):
-    t = np.arange(0, length, (dt*CHUNK))
+def plot_data(array1, array2, array3, t):
+    # t = np.arange(0, length, (dt*CHUNK))
 
     # plot data
-    plt.plot(t, array, color='c')
+    plt.plot(t, array1, color='r', label="clean")
+
+    plt.plot(t, array2, color='b', label="clean+noise")
+
+    plt.plot(t, array3, color='g', label="noise")
 
     # x-axis label
     plt.xlabel("Time (sec)")
@@ -171,6 +247,15 @@ def plot_data(array, dt, length):
 
     # show the plot
     plt.show()
+
+
+def add_noise(f, t):
+    noise = 2.5*np.random.randn()
+    f_noise = f + noise
+    print("f_noise= ", f_noise)
+    f_return = np.frombuffer(f_noise, np.int16)[0]
+    print("f_return= ", f_return)
+    return f_return
 
 
 if __name__ == '__main__':
