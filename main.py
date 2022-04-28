8fab2395d04d3f37de0f9a783770e90152e4e977
DarrenCoppinger <darrencoppinger@gmail.com>
2022-04-28 16:59:48 +0100

    add simple FFT function

--- main.py
+++ main.py
@@ -196,7 +196,6 @@ def fft_filter():
     new_CHUNK = 1
     # read first byte CHUNK of wav file
     file_data = wavefile.readframes(new_CHUNK)
-    print("file_data= ",file_data)
 
     # create matrix of wave data
     fft_input_array = []
@@ -209,14 +208,16 @@ def fft_filter():
     while file_data != b'':
         try:
             # covert byte data to integer data
-            input_data_int = np.frombuffer(file_data, np.int16)
+            # input_data_int = np.frombuffer(file_data, np.int16)
+            input_data_int = np.frombuffer(file_data, dtype=np.int16)
 
             # add integer data to array
             fft_input_array.append(input_data_int[0])
 
             # create noise
             # Note on len(input_data_int). Last CHUNK might not be a full CHUNK.
-            noise = np.random.randint(-1000, 1000, len(input_data_int))
+            # noise = np.random.randint(-1000, 1000, len(input_data_int))
+            noise = 5000*np.random.randn(len(input_data_int))
 
             # output noise data to array
             fft_noise_array.append(noise[0])
@@ -251,64 +252,199 @@ def fft_filter():
 
     print("End of WAV file")
 
-    # plot input wave
+    # # plot input wave
+    t = np.arange(0, w_len, ((1 / RATE) * new_CHUNK))
+
     n = len(fft_input_plus_noise_array)
     fhat = np.fft.fft(fft_input_plus_noise_array, n)
     PSD = fhat * np.conj(fhat) / n
-
     freq = (1 / (dt * n)) * np.arange(n)
     L = np.arange(1, np.floor(n / 2), dtype='int')
 
+    n_input = len(fft_input_array)
+    fhat_input = np.fft.fft(fft_input_array, n_input)
+    PSD_input = fhat * np.conj(fhat_input) / n_input
+    freq_input = (1 / (dt * n_input)) * np.arange(n_input)
+    L_input = np.arange(1, np.floor(n_input / 2), dtype='int')
+
     # find frequencies with large powers
-    indices = PSD > 0.5
+    indices = PSD > 1
     PSD_clean = PSD * indices
     fhat = indices * fhat
     # Inverse FFT for filtered signal
     ffilt = np.fft.ifft(fhat)
 
-    fig, axs = plt.subplots(3, 1)
+    # fig, axs = plt.subplots(3, 1)
+    #
+    # print("w_len= ", w_len)
+    # print("WIDTH= ", WIDTH)
+    # print("RATE= ", RATE)
+    # print("t= ", len(t))
+    # print("fft_input_array= ", len(fft_input_array))
+    # print("fft_input_plus_noise_array= ", len(fft_input_plus_noise_array))
+    #
+    # plt.sca(axs[0])
+    # plt.plot(t, fft_input_plus_noise_array, color='k', label='noisy')
+    # plt.plot(t, fft_input_array, color='c', label='clean')
+    # # plt.xlim(2, 2.2)
+    # plt.legend()
+    #
+    # plt.sca(axs[1])
+    # plt.plot(t, ffilt, color='k', label="filtered")
+    # plt.xlim(2, 2.2)
+    # plt.legend()
+    #
+    # plt.sca(axs[2])
+    #
+    # plt.plot(freq[L], PSD[L], color='k', label='input + noise')
+    # plt.plot(freq_input[L_input], PSD_input[L_input], color='c', label='input')
+    # # plt.plot(freq[L], PSD_clean[L], color='k', label='Filtered')
+    # plt.xlim(freq[L[0]], freq[L[-1]])
+    # # plt.xlim(freq[L[0]], 1000)
+    # # plt.ylim(0, 2)
+    # plt.legend()
+    #
+    # plt.show()
 
-    t = np.arange(0, w_len, ((1 / RATE) * new_CHUNK))
-    print("w_len= ", w_len)
-    print("WIDTH= ", WIDTH)
-    print("RATE= ", RATE)
-    print("t= ", len(t))
-    print("fft_input_array= ", len(fft_input_array))
-    print("fft_input_plus_noise_array= ", len(fft_input_plus_noise_array))
+
+    dt_f = 0.001
+    t_f = np.arange(0, 1, dt_f)
+    f = np.sin(2*np.pi*50*t_f) + np.sin(2*np.pi*120*t_f)
+    f_clean = f
+    f = f + 2.5*np.random.randn(len(t_f))
+
+    n_f = len(t_f)
+    fhat_f = np.fft.fft(f, n_f)
+    PSD_f = fhat_f * np.conj(fhat_f) / n_f
+    freq_f = (1 / (dt_f * n_f)) * np.arange(n_f)
+    L_f = np.arange(1, np.floor(n_f / 2), dtype='int')
+
+    # find frequencies with large powers
+    indices_f = PSD_f > 100
+    PSD_clean_f = PSD_f * indices_f
+    fhat_f = indices_f * fhat_f
+    # Inverse FFT for filtered signal
+    ffilt_f = np.fft.ifft(fhat_f)
+
+    fig, axs = plt.subplots(4, 1)
 
     plt.sca(axs[0])
-    plt.plot(t, fft_input_plus_noise_array, color='k', label='noisy')
-    plt.plot(t, fft_input_array, color='c', label='clean')
-    plt.xlim(2, 2.2)
+    plt.plot(t_f, f, color='k', label='noisy')
+    plt.plot(t_f, f_clean, color='c', label='clean')
+    plt.xlim(t_f[0], t_f[-1])
     plt.legend()
 
     plt.sca(axs[1])
-    plt.plot(t, ffilt, color='k', label="filtered")
-    plt.xlim(2, 2.2)
+    plt.plot(t_f, ffilt_f, color='k', label="filtered")
+    plt.xlim(t_f[0], t_f[-1])
     plt.legend()
 
     plt.sca(axs[2])
-    plt.plot(freq[L], PSD[L], color='c', label='Noisy')
-    plt.plot(freq[L], PSD_clean[L], color='k', label='Filtered')
-    # plt.xlim(freq[L[0]], freq[L[-1]])
-    plt.xlim(freq[L[0]], 600)
-    plt.xlim(0, 2)
+    plt.plot(freq_f[L_f], PSD_f[L_f], color='k', label='noisy')
+    plt.plot(freq_f[L_f], PSD_clean_f[L_f], color='c', label='filtered')
+    # plt.plot(freq[L], PSD_clean[L], color='k', label='Filtered')
+    plt.xlim(freq_f[L_f[0]], freq_f[L_f[-1]])
+    # plt.xlim(freq[L[0]], 1000)
+    # plt.ylim(0, 2)
+    plt.legend()
+
+    plt.sca(axs[3])
+    plt.plot(freq_f[L_f], PSD_clean_f[L_f], color='c', label='filtered')
+    plt.xlim(freq_f[L_f[0]], freq_f[L_f[-1]])
     plt.legend()
 
     plt.show()
 
     # create output file
-    wf = wave.open("FFT_OUTPUT.wav", 'wb')
-    wf.setnchannels(CHANNELS)
-    wf.setsampwidth(WIDTH)
-    wf.setframerate(RATE)
-    wf.writeframes(b''.join(fft_output_frames))
-    wf.close()
+    print("FFT Output RATE= ", RATE)
+    wf_output = wave.open("FFT_OUTPUT.wav", 'wb')
+    wf_output.setnchannels(wavefile.getnchannels())
+    wf_output.setsampwidth(wavefile.getsampwidth())
+    wf_output.setframerate(wavefile.getframerate())
+    wf_output.writeframes(b''.join(fft_output_frames))
+    wf_output.close()
 
     # Terminate PyAudio object
     p.terminate()
     # Exit programme
     sys.exit()
+
+
+def fft_simple_filter():
+    print("fft_simple_filter")
+    # Read in WAV file
+
+    dt_f = 0.001
+    t_f = np.arange(0, 1, dt_f)
+    f = np.sin(2*np.pi*50*t_f) + np.sin(2*np.pi*120*t_f)
+    f_clean = f
+    f = f + 2.5*np.random.randn(len(t_f))
+
+    n_f = len(t_f)
+    fhat_f = np.fft.fft(f, n_f)
+    PSD_f = fhat_f * np.conj(fhat_f) / n_f
+    freq_f = (1 / (dt_f * n_f)) * np.arange(n_f)
+    L_f = np.arange(1, np.floor(n_f / 2), dtype='int')
+
+    # find frequencies with large powers
+    indices_f = PSD_f > 100
+    PSD_clean_f = PSD_f * indices_f
+    fhat_f = indices_f * fhat_f
+    # Inverse FFT for filtered signal
+    ffilt_f = np.fft.ifft(fhat_f)
+
+    fig, axs = plt.subplots(4, 1)
+
+    plt.sca(axs[0])
+    plt.plot(t_f, f, color='k', label='noisy')
+    plt.plot(t_f, f_clean, color='c', label='clean')
+    plt.xlim(t_f[0], t_f[-1])
+    plt.legend()
+
+    plt.sca(axs[1])
+    plt.plot(t_f, ffilt_f, color='k', label="filtered")
+    plt.xlim(t_f[0], t_f[-1])
+    plt.legend()
+
+    plt.sca(axs[2])
+    plt.plot(freq_f[L_f], PSD_f[L_f], color='k', label='noisy')
+    plt.plot(freq_f[L_f], PSD_clean_f[L_f], color='c', label='filtered')
+    # plt.plot(freq[L], PSD_clean[L], color='k', label='Filtered')
+    plt.xlim(freq_f[L_f[0]], freq_f[L_f[-1]])
+    # plt.xlim(freq[L[0]], 1000)
+    # plt.ylim(0, 2)
+    plt.legend()
+
+    plt.sca(axs[3])
+    plt.plot(freq_f[L_f], PSD_clean_f[L_f], color='c', label='filtered')
+    plt.xlim(freq_f[L_f[0]], freq_f[L_f[-1]])
+    plt.legend()
+
+    plt.show()
+
+
+    # Terminate PyAudio object
+    p.terminate()
+    # Exit programme
+    sys.exit()
+
+
+# from https://stackoverflow.com/questions/33933842/how-to-generate-noise-in-frequency-range-with-numpy
+def fftnoise(f):
+    f = np.array(f, dtype="complex")
+    Np = (len(f) - 1) // 2
+    phases = np.random.rand(Np) * 2 * np.pi
+    phases = np.cos(phases) + 1j * np.sin(phases)
+    f[1 : Np + 1] *= phases
+    f[-1 : -1 - Np : -1] = np.conj(f[1 : Np + 1])
+    return np.fft.ifft(f).real
+
+
+def band_limited_noise(min_freq, max_freq, samples=1024, samplerate=1):
+    freqs = np.abs(np.fft.fftfreq(samples, 1 / samplerate))
+    f = np.zeros(samples)
+    f[np.logical_and(freqs >= min_freq, freqs <= max_freq)] = 1
+    return fftnoise(f)
 
 
 def record():
@@ -361,10 +497,10 @@ def record():
 
 
 def read_file():
-    name = input("Enter WAV file name: ")
+    # name = input("Enter WAV file name: ")
     try:
         # wf = wave.open(name, 'r')
-        wf = wave.open(name, 'r')
+        wf = wave.open('darren.wav', 'r')
     except wave.Error:
         print("You must input a WAV audio file")
         sys.exit()
@@ -437,7 +573,8 @@ if __name__ == '__main__':
     print("2: WAV File Active Noise Cancelling")
     print("3: Play WAV File ")
     print("5: Record audio .wav File ")
-    mode = input("Enter mode number: ")
+    # mode = input("Enter mode number: ")
+    mode = '3'
     if mode == '1':
         print("1: Live Active Noise Cancellation")
         live_anc()
